0cf050e07dee176206ed35dcf2ff41f87c51faff
Michelle <mxie97@gmail.com>
2023-11-21 12:59:06 -0800

    backtracking Maze generator algorithm working

--- client/components/App.js
+++ client/components/App.js
@@ -1,13 +1,96 @@
 import React, { Component } from 'react';
 import Maze from './Maze';
 
-function getInitialState() {
-  const arrayColors = ['white', 'black'];
-  const mazeResults = [];
-  for (let i = 0; i < 100; i++) {
-    mazeResults.push(arrayColors[Math.round(Math.random())]);
+//r(integer): row of cell of interest
+//c(integer): column of cell of interest
+//grid: 2D maze grid
+//looking for neighbors that are walls
+//return list of all neighboring cells that match our request
+function findNeighbors(r, c, grid) {
+  const neighbors = [];
+  if (r > 1 && grid[r - 2][c]) {
+    neighbors.push([r - 2, c]);
+  }
+  if (r < 9 && grid[r + 2][c]) {
+    neighbors.push([r + 2, c]);
+  }
+  if (c > 1 && grid[r][c - 2]) {
+    neighbors.push([r, c - 2]);
+  }
+  if (c < 9 && grid[r][c + 2]) {
+    neighbors.push([r, c + 2]);
+  }
+  return neighbors;
+}
+
+//h = w = 5 * 60px
+//H = W = 11 * 60px
+// - is 1 (wall)
+//grid=[
+//[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+//[1, 0, 1, n, 1, 1, 1, 1, 1, 1, 1],
+//[1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+//[1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+//[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+//[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+//[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+//[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+//[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+//[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+//[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+//]
+
+function backtrackingMazeGenerator() {
+  const grid = [];
+  const row = [];
+  for (let columns = 0; columns < 11; columns++) {
+    row.push(1);
+  }
+  for (let rows = 0; rows < 11; rows++) {
+    grid.push([...row]);
   }
 
+  //start at cell at row index 1, column index 1 for now. can set randomly later if you want
+  let currentRow = 1;
+  let currentColumn = 1;
+  let track = [[1, 1]];
+  grid[currentRow][currentColumn] = 0;
+
+  while (track.length > 0) {
+    currentRow = track[track.length - 1][0];
+    currentColumn = track[track.length - 1][1];
+    let neighbors = findNeighbors(currentRow, currentColumn, grid);
+
+    if (neighbors.length === 0) {
+      track.pop();
+    } else {
+      let randomIndex = Math.floor(Math.random() * neighbors.length);
+      let neighborRow = neighbors[randomIndex][0];
+      let neighborColumn = neighbors[randomIndex][1];
+      //pick a random neighboring cell and set to 0 in grid
+      grid[neighborRow][neighborColumn] = 0;
+      //clear wall in between neighboring cell and current cell
+      grid[Math.floor(neighborRow + currentRow) / 2][
+        Math.floor(neighborColumn + currentColumn) / 2
+      ] = 0;
+
+      track.push([neighborRow, neighborColumn]);
+    }
+  }
+  return grid;
+}
+
+function getInitialState() {
+  const arrayColors = ['white', 'black'];
+  const generatedMaze = backtrackingMazeGenerator();
+  const mazeResultsBinary = [];
+  for (let row = 0; row < generatedMaze.length; row++) {
+    mazeResultsBinary.push(...generatedMaze[row]);
+  }
+  const mazeResults = mazeResultsBinary.map((number) => {
+    if (number === 1) return 'black';
+    else return 'white';
+  });
   return {
     maze: mazeResults,
   };
--- client/components/Maze.js
+++ client/components/Maze.js
@@ -2,7 +2,12 @@ import React from 'react';
 
 const Maze = (props) => {
   const { maze } = props;
-  const cells = maze.map((color) => <div className={color}></div>);
+  const cells = maze.map((color, index) => (
+    <div
+      className={color}
+      key={index}
+    ></div>
+  ));
   return <div className='maze-container'>{cells}</div>;
 };
 
