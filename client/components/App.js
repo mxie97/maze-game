cfa722955567fd0df72b3c33cd8aba3571e2bd6a
Michelle <mxie97@gmail.com>
2023-11-21 07:49:25 -0800

    project is rendering on local host

---
+++ client/components/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        <div>hello</div>
+        <button>click me</button>
+      </div>
+    );
+  }
+}
+
+export default App;
